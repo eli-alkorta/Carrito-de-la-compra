1fe9fc56fab961beef82a30a188c0bcdd43d6652
eli-alkorta <elialkor@gmail.com>
2020-05-15 17:30:48 +0200

    first and last push

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Carrito-de-la-compra
+# Carrito de la compra
+
+![Carrito de la compra](/img/shopping-cart.png)
---
+++ js/app.js
@@ -0,0 +1,122 @@
+'use strict';
+
+let shoppingCart = document.querySelector('#cart');
+let courseList = document.querySelector('#course-list');
+let localCourses = document.querySelector('#cart-list tbody');
+let resetCourses = document.querySelector('#empty-cart');
+
+let idArr=[];
+
+function addCourse(event){
+    
+    if(event.target.classList.contains("agregar-carrito")){
+        let chosenCourse = event.target.parentElement.parentElement;
+        readCourse(chosenCourse);
+    }
+}
+
+function readCourse(chosenCourse){
+    //Crea un objeto con los datos de cada curso seleccionado//
+    const courseCard = {
+        image: chosenCourse.querySelector('img').src,
+        title: chosenCourse.querySelector('h4').innerText,
+        price: chosenCourse.querySelector('.price span').innerText,
+        id: chosenCourse.querySelector('a').getAttribute('data-id')
+    }
+    
+    let chosenId = parseInt(courseCard.id);
+    
+    if (idArr.indexOf(chosenId) === -1) {
+        idArr.push(chosenId); 
+        renderInCart(courseCard);
+        
+    } else {
+        idArr.splice(chosenId, 1);    
+    }   
+}
+
+function renderInCart(courseCard){
+    //Pinta el curso seleccionado en el carrito//
+    const row = document.createElement('tr');
+    row.innerHTML = `<td><img src="${courseCard.image}" width=100></td>
+    <td>${courseCard.title}</td>
+    <td>${courseCard.price}</td>
+    <td><a href=# class="delete-course" data-id=${courseCard.id}>X</a></td>`;
+    localCourses.appendChild(row);
+    shoppingCart.addEventListener('click', deleteCourse);
+    setInLocal(courseCard);
+}
+
+function deleteCourse(event){
+    let course;
+    let courseId;
+    if(event.target.classList.contains("delete-course")){
+        event.target.parentElement.parentElement.remove();
+        course = event.target.parentElement.parentElement;
+        courseId = course.querySelector('a').getAttribute('data-id');
+        
+    }
+    deleteFromLocal(courseId);
+}
+
+function deleteFromLocal(courseId){
+    let coursesLS;
+    coursesLS = getFromLocal();
+    coursesLS.forEach(function(course, index){
+        if(course.id === courseId){
+            coursesLS.splice(index, 1);
+        }
+    })
+    localStorage.setItem('courses', JSON.stringify(coursesLS));
+}
+
+function resetAllCourses(){
+    
+    while(localCourses.firstChild){
+        localCourses.removeChild(localCourses.firstChild);
+    }
+    emptyLocal();
+    
+    return false;
+}
+
+function emptyLocal(){
+    localStorage.clear();
+}
+
+function setInLocal(courseCard){
+    let courses;
+    courses = getFromLocal();
+    courses.push(courseCard);
+    localStorage.setItem('courses', JSON.stringify(courses));
+    
+}
+
+function getFromLocal(){
+    let coursesLS;
+    if(localStorage.getItem('courses') === null){
+        coursesLS = [];
+    }else{
+        coursesLS = JSON.parse(localStorage.getItem('courses'));
+    }
+    return coursesLS;
+}
+
+function readLocal(){
+    let coursesLS;
+    coursesLS = getFromLocal();
+    coursesLS.forEach(function(courseCard){
+        const row = document.createElement('tr');
+        row.innerHTML = `<td><img src="${courseCard.image}" width=100></td>
+        <td>${courseCard.title}</td>
+        <td>${courseCard.price}</td>
+        <td><a href=# class="delete-course" data-id=${courseCard.id}>X</a></td>`;
+        localCourses.appendChild(row);
+        shoppingCart.addEventListener('click', deleteCourse);
+    })
+}
+
+
+window.addEventListener('load', readLocal)
+resetCourses.addEventListener('click', resetAllCourses);
+courseList.addEventListener('click', addCourse);
